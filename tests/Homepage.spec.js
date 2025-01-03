d36e3e68ed190a18dc531cf49d09682effd0e541
giri2k01 <giridharan2k01@gamil.com>
2025-01-03 08:13:04 +0530

    push to master

---
+++ tests/Homepage.spec.js
@@ -0,0 +1,27 @@
+let {test,expect}=require('@playwright/test')  
+//import {test,expect} from '@playwright/test'     //importing test & exepct from playwright/test
+
+test('Home page', async ({ page }) =>{                    //makes a program return a promise //Home page is the name of the test // {}=> is the declaration of ananiomus function //page is a fixture
+ await page.goto('https://letcode.in')                 //makes the func wait for the promise(wait until the page is loaded)
+ await page.setViewportSize({ width: 1920, height: 1080 })
+ await page.click('id=testing')
+ await page.locator('a[href="/edit"]').click();
+ const pagename = await page.locator('//h1[contains(text(),"Input")]')
+ await expect(pagename).toBeVisible()
+ await page.fill('xpath=//*[@id="fullName"]','Giri')
+ const fields = await page.$$('xpath=//div[@class="field"]')
+//await page.fill('xpath=//*[@id="fullName"]', 'Giri');  // Using XPath with page.fill
+//const fields = await page.$$('xpath=//div[@class="field"]');  // Using XPath with page.$$
+for(const field of fields ){
+    const res = await field.textContent()
+    console.log(res)
+}
+ 
+ //var pagetitle = page.title();
+ //console.log('pagetitle:',pagetitle );
+ //await expect(page).toHaveTitle('Google');
+ //const ppgeurl = page.url();
+ //page.close();
+})
+
+
